0ca3075ea97a3a8bd60897576f8067f8c93122bb
Sheikh Shihab Ahmed <shihabahameds4@gmail.com>
2026-01-13 02:31:47 +0600

    activitylog & expireddocs complete

--- resources/js/components/widgets/ActivityLog.vue
+++ resources/js/components/widgets/ActivityLog.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
-import { ref, computed,inject } from 'vue';
+import { ref, computed, inject } from 'vue';
 import axios from 'axios';
 import listrow from '../listrow.vue';
 import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 import 'vue3-perfect-scrollbar/style.css'
 import ActivityLog from './ActivityLog.vue';
-import { Documents, Actions, Users } from '@/dbschema';
+import { Actions, Users } from '@/dbschema';
 import { route, Route } from 'ziggy-js';
-
-
-
 
 let labelstyle = inject('labelstyle');
 
-let Recent = ref<Documents[]>([]);
-const documents = inject<Documents[]>('documents', []);
-
-const allActivities = computed(() => [...documents, ...Recent.value]);
-
+let Recent = ref<Actions[]>([]);
+const actions = inject<Actions[]>('actions', []);
 
 function getclrcls(evnt_type: string) {
     switch (evnt_type) {
@@ -36,21 +30,9 @@ function getclrcls(evnt_type: string) {
     }
 }
 
-// async function dbuncheck(actid: number) {
-//     try {
-
-//         let res = await axios.post(route('checked', {}, undefined, Ziggy), { actid });
-
-//         console.log('res',res.data.error);
-
-//     } catch (error: any) {
-//         console.log('exeption->',error,actid)
-//     }
-// }
-
 async function dbuncheck(actid: number) {
     try {
-        const res = await axios.post(route('checked'), { 'actid':actid });
+        const res = await axios.post(route('checked'), { 'actid': actid });
 
         console.log('dbuncheck response:', res.data);
         return res; // return it for awaiting
@@ -66,30 +48,14 @@ async function dbuncheck(actid: number) {
     }
 }
 
-
-// function uncheck(id: number) {
-//     let trgtelmt = allActivities.value.find(doc => doc.user.action.find(b => b.id === id))
-
-//     if (trgtelmt) {
-//         const trgtact = trgtelmt.user.action.find(actobj => actobj.id === id);
-//         trgtact ? trgtact.checked = 1 : trgtact
-//         console.log('trgtact->',trgtact?.checked)
-//         console.log('function->',dbuncheck(id))
-
-//     }
-// }
-
-
 async function uncheck(id: number) {
-    let trgtelmt = allActivities.value.find(doc => doc.user?.action?.some(act => act.id === id));
+    let trgtelmt = actions.find(act => act.id === id)
 
-    if (!trgtelmt || !trgtelmt.user?.action) return; // safety check
+    if (!trgtelmt) return; // safety check
 
-    const trgtact = trgtelmt.user.action.find(act => act.id === id);
-
-    if (trgtact) {
-        trgtact.checked = 1; // update local state
-        console.log('trgtact.checked->', trgtact.checked,'trgtact->', trgtact,'actid->', id,'dashboard->',route('checked'));
+    if (trgtelmt) {
+        trgtelmt.checked = 1; // update local state
+        console.log('trgtelmt.checked->', trgtelmt.checked, 'trgtelmt->', trgtelmt, 'actid->', id, 'dashboard->', route('checked'));
         try {
             const res = await dbuncheck(id);
             console.log('dbuncheck response:', res?.data);
@@ -107,44 +73,55 @@ let listrowstyle = 'flex flex-col gap-1'
     <div class="h-full flex flex-col border border-white rounded-md p-2 ">
         <p :class="labelstyle">Activity Log </p>
         <!--  -->
-        <!-- Scrollable container :href="doc.link"-->
+        <!-- Scrollable container :href="act.link"-->
         <PerfectScrollbar class="flex-1 mt-2">
             <listrow class="flex flex-col gap-1">
-                <div v-if="allActivities.length">
-                    <div id="unseen" v-for="doc in allActivities" :key="doc.id">
-                        <div v-for="act in doc.user.action" :key="act.id">
-                            <div v-if="act.checked === 0">
+                <div v-if="Recent.length">
+                    <div id="unseen" v-for="act in Recent" :key="act.id">
+                        <a href="#" @click="uncheck(act.id)"
+                            class="px-3 text-[0.5em] sm:text-[0.6em] md:text-[0.6em] lg:text-[0.65em] xl:text-[0.7em] font-bold py-1 text-black border block rounded-md relative"
+                            :class="getclrcls(act.event_type)">
+                            {{ act.filename }} was {{ act.event_type }} by {{ act.user.name }} ({{ act.user.role
+                            }})
+                            <br>
+                            {{ act.created_at }}
+                            <div
+                                class="right-[5px] top-[5px] size-[6px] rounded-xl absolute border-1 border-white  bg-red-500">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div v-if="actions.length">
+                    <div id="unseen" v-for="act in actions" :key="act.id">
+                        <div v-if="act.checked === 0">
+                            <a href="#" @click="uncheck(act.id)"
+                                class="px-3 text-[0.5em] sm:text-[0.6em] md:text-[0.6em] lg:text-[0.65em] xl:text-[0.7em] font-bold py-1 text-black border block rounded-md relative"
+                                :class="getclrcls(act.event_type)">
+                                {{ act.filename }} was {{ act.event_type }} by {{ act.user.name }} ({{ act.user.role
+                                }})
+                                <br>
+                                {{ act.created_at }}
+                                <div
+                                    class="right-[5px] top-[5px] size-[6px] rounded-xl absolute border-1 border-white  bg-red-500">
+                                </div>
+                            </a>
+                        </div>
+                        <div v-else>
+                        </div>
+                    </div>
+                    <div id="seen" v-for="act in actions" :key="act.id">
+                        <div v-if="actions.length">
+                            <div v-if="act.checked === 1">
                                 <a href="#" @click="uncheck(act.id)"
                                     class="px-3 text-[0.5em] sm:text-[0.6em] md:text-[0.6em] lg:text-[0.65em] xl:text-[0.7em] font-bold py-1 text-black border block rounded-md relative"
                                     :class="getclrcls(act.event_type)">
-                                    {{ act.filename }} was {{ act.event_type }} by {{ doc.user.name }} ({{ doc.user.role
-                                    }})
+                                    {{ act.filename }} was {{ act.event_type }} by {{ act.user.name }} ({{
+                                        act.user.role }})
                                     <br>
                                     {{ act.created_at }}
-                                    <div
-                                        class="right-[5px] top-[5px] size-[6px] rounded-xl absolute border-1 border-white  bg-red-500">
-                                    </div>
                                 </a>
                             </div>
                             <div v-else>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="seen" v-for="doc in allActivities" :key="doc.id">
-                        <div v-if="allActivities.length">
-                            <div v-for="act in doc.user.action" :key="act.id">
-                                <div v-if="act.checked === 1">
-                                    <a href="#" @click="uncheck(act.id)"
-                                        class="px-3 text-[0.5em] sm:text-[0.6em] md:text-[0.6em] lg:text-[0.65em] xl:text-[0.7em] font-bold py-1 text-black border block rounded-md relative"
-                                        :class="getclrcls(act.event_type)">
-                                        {{ doc.filename }} was {{ act.event_type }} by {{ doc.user.name }} ({{
-                                            doc.user.role }})
-                                        <br>
-                                        {{ act.created_at }}
-                                    </a>
-                                </div>
-                                <div v-else>
-                                </div>
                             </div>
                         </div>
                         <div v-else>
--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 import PendingDocs from '@/components/widgets/PendingDocs.vue';
 import ActivityLog from '@/components/widgets/ActivityLog.vue';
-import ExpDocs from '@/components/widgets/ExpDocs.vue';
+import ExpiredDocs from '@/components/widgets/ExpiredDocs.vue';
 import { provide, ref } from 'vue';
-import { Documents } from '@/dbschema';
+import { Documents,Actions } from '@/dbschema';
 
 
 var cls: String = 'container  p-2 border border-white border-5 rounded-xl'
 provide('labelstyle', 'text-white text-[0.7em] font-bold')
 
 
-const  props  = defineProps<{ documents?: Documents[] }>()
+const  props  = defineProps<{ documents?: Documents[],actions?: Actions[] }>()
 provide('documents', props.documents ?? [])
+provide('actions', props.actions ?? [])
 </script>
 
 <template>
     <div class="col-start-4 lg:col-start-3 md:col-start-3 row-span-14 col-end-17 container p-2 gap-3 grid  grid-cols-2 grid-rows-2">
         <ActivityLog class="col-start-1 col-end-2 row-span-2" />
         <PendingDocs class="col-start-2 col-end-4 row-span-1" />
-        <ExpDocs class="col-start-2 col-end-4 row-span-1" />
+        <ExpiredDocs class="col-start-2 col-end-4 row-span-1" />
     </div>
 </template>
 
